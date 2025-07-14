90192fdca384b1b36006cd0bbeceb97c34bdcb0b
Lif Siregar <khalifsiregar123@gmail.com>
2025-07-14 16:49:10 +0700

    update config vite

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Ganti ini sesuai nama repo kamu
-const repoName = 'Fourteendecy'
+
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, // penting biar link asset bener
+  base: '/Fourteendecy/',
+
 })
