0e9131b4675c914d85372c06a9cde9f51091e996
Lif Siregar <khalifsiregar123@gmail.com>
2025-07-14 17:01:16 +0700

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,16 @@ import './index.css';          // tailwind imports
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Fourteendecy">
-<Routes>
-  <Route index element={<App />} />          {/* loader */}
-  <Route path="home" element={<Home />} />   {/* konten */}
-  <Route path="*" element={<Navigate to="home" replace />} />
-</Routes>
+    <BrowserRouter>
+      <Routes>
+        {/* initial route menampilkan App (loading lalu redirect) */}
+        <Route path="/" element={<App />} />
+        {/* tujuan akhir */}
+        <Route path="/home" element={<Home />} />
 
+        {/* fallback: apa pun selain rute di atas -> home */}
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 );
